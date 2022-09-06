4b1f7ab58ac5517a94f8fce2106b0d77a10b3e7b
unknown <alyssoncolombo@outlook.com>
2022-09-06 15:10:51 -0300

    feat: update serializers, views, urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
-from accounts.views import AcccountDetailView, AccountView
+from accounts.views import AcccountDetailView, AccountView, UpgradeToAdminOrStaff, DesactivateAccount, CreateEmployee
 from rest_framework.authtoken import views
 from django.urls import path
 
 urlpatterns = [
-    path("accounts/login/", views.obtain_auth_token),
-    path("accounts/", AccountView.as_view()),
-    path("accounts/<pk>/", AcccountDetailView.as_view()),
+    path("login/", views.obtain_auth_token),
+    path("", AccountView.as_view()),
+    path("employee/", CreateEmployee.as_view()),
+    path("<pk>/", AcccountDetailView.as_view()),
+    path("<pk>/update-permissions/", UpgradeToAdminOrStaff.as_view()),
+    path("<pk>/management/", DesactivateAccount.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,41 @@
-from accounts.serializers import SerializerAccounts
+from accounts.serializers import SerializerAccounts, UpgradeToAdminOrStaff, Desactivate, SerializerEmployee
 from rest_framework import generics
 from .models import Account
 from rest_framework.authentication import TokenAuthentication
-from .permissions import UpdateAndDelete
+from .permissions import UpdateAndDelete, OnlyAdmin, ReadOnlyAdmin
 
-
-# Create your views here.
+#Cria user comum (Não precisa de permissão), Lista todos users (apenas admin)
 class AccountView(generics.ListCreateAPIView):
-
+    authentication_classes = [TokenAuthentication]        
+    permission_classes = [ReadOnlyAdmin]
     queryset = Account.objects.all()
     serializer_class = SerializerAccounts
     
+#Cria funcionario(apenas admin)
+class CreateEmployee(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]        
+    permission_classes = [OnlyAdmin]
+    queryset = Account.objects.all()
+    serializer_class = SerializerEmployee
     
+# Visualiza, atualiza ou deleta user apenas se for admin ou o própio user   
 class AcccountDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]        
-    permission_classes = [ UpdateAndDelete]    
+    permission_classes = [UpdateAndDelete]    
     queryset = Account.objects.all()
     serializer_class = SerializerAccounts
     
+# Admin atualiza qualquer user como funcionário ou admin   
+class UpgradeToAdminOrStaff(generics.UpdateAPIView):
+    authentication_classes = [TokenAuthentication]        
+    permission_classes = [OnlyAdmin]    
+    queryset = Account.objects.all()
+    serializer_class = UpgradeToAdminOrStaff   
+    
+# Admin pode desativar funcionário    
+class DesactivateAccount(generics.UpdateAPIView):
+    authentication_classes = [ TokenAuthentication]        
+    permission_classes = [OnlyAdmin]    
+    queryset = Account.objects.all()
+    serializer_class = Desactivate 
     
